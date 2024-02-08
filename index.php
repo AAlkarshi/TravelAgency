58218d9a42ef1e844bbdf0570d539f50b5925e57
Abdullrahman Al karshi <alkarshi.abdullrahman@gmail.com>
2024-02-08 15:57:14 +0100

    Site de Voyage

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+	<title> Reprise</title>
+</head>
+
+<body>
+	
+<div id="OngletHaut">
+	<h2 id="TitreFormulaire">Travel Agency<strong id="Point">.</strong></h2>
+
+	<div id="Onglet">
+		<p> <a href="#"> Accueil  </a> </p>
+		<p> <a href="#"> Destinations </a> </p>
+		<p> <a href="#"> Circuits </a> </p>
+		<p> <a href="#"> Contact  </a> </p>
+	</div>
+</div>
+
+
+<br>
+
+
+<!--  IMG DE FOND ET BOUTON  -->
+
+<section>
+		<div id="IMAGE"> 
+			<img src="main.jpg" height="100%" width="100%">
+			<p id="PhraseAccroche"><b>ORGANISER VOTRE VOYAGE SUR MESURE </b></p>
+			<br>
+			<button id="BtnAccueil" type="submit">
+				<div>
+					<p> Par ici </p>
+				</div>
+			</button>
+		</div>
+</section>
+
+
+<!--  ICONE ET IMAGE  -->
+
+<section>
+	
+<div id="ICONE">
+
+	<div>
+		<img class="ICON" src="steps-icon-1.png">
+		<br>
+		<br>
+		<p class="TITRE_ICONE"> PLANIFIER </p>
+		<p class="TEXTE_ICONE"> Confiez-nous vos rêves d'évasion : en famille <br> ou entre amis, nous trouverons la formule qui <br> comblera vos attentes.
+		</p>
+	</div>
+
+	<div>
+		<img class="ICON" src="steps-icon-2.png">
+			<br>
+			<br>
+		<p class="TITRE_ICONE"> ORGANISER </p>
+		<p class="TEXTE_ICONE"> Bénéficiez de l'expertise de nos spécialistes <br> pour chaque destinations, ils vous accompagnent dans la réalisation de votre voyage.
+		</p>
+	</div>
+
+	<div>
+		<img class="ICON" src="steps-icon-3.png">
+		<br>
+		<br>
+		<p class="TITRE_ICONE"> VOYAGER </p>
+		<p class="TEXTE_ICONE">
+			Nous nous chargerons d'assumer votre <br> sécurité
+			et de veiller à votre sérénité <br> tout au long de votre voyage.
+		</p>
+	</div>
+</div>
+
+</section>
+
+<br> 
+
+<!-- SECTION GRISE -->
+
+<section id="SectionGrisé">
+		<div id="RegroupementSection">
+		
+
+			<article id="SectionPartezEnFamille">
+				<div id="TextePartezEnFamille">
+					<h3> <strong>PARTEZ EN<br>FAMILLE<br> </strong> </h3>
+					<hr>
+				<p> <small> Offrez le meilleur à ceux que vous aimez et partagez des moments fabuleux ! </small></p>
+				</div>
+					<button class="BtnInfos" type="submit">
+						<p> Plus d'infos </p>
+					</button>
+			</article>
+		
+
+			<article id="SectionEnvieDeEvader">
+				<div id="TexteEnvieDeSEvader">
+					<h3> <strong>ENVIE <br> DE<br>S'EVADER<br> </strong> </h3>
+					<hr>
+						<p> <small> Parfois un peu d'évasion serait le bienvenue et ferait le plus grand bien ! </p> </small>
+				</div>
+					<button class="BtnInfos" type="submit">
+						<p> Plus d'infos </p>
+					</button>
+			</article>
+
+		</div>
+</section>
+
+
+
+<br>
+
+
+<!-- SECTION CONTACT -->
+
+<section>
+	<h3 class="text-center" 
+	style="border-bottom: 3px solid orange;
+		   margin-bottom: 20px;
+		   width: 400px;
+		   margin: 0 auto 20px;
+
+
+		   "> 
+		CONTACTEZ-NOUS 
+	</h3>
+
+<p class="text-center"  style="margin: 0 50px 0 50px; "> 
+Chez Travel Agency nous savons que voyager est une aventure humaine mais également un engagement financier important pour vous. C'est pourquoi nous mettons un point d'honneur à prendre en compte chacune de vos attentes pour vous aider dans la préparation de votre séjour, circuit ou voyage sur mesure. </p>
+
+<br> 
+<form class="text-center" style="border-radius: 25px;">
+	
+		<input type="text" name="ContactNOM" placeholder="Votre nom">
+			<br>
+	
+		<input type="text" name="ContactPRENOM" placeholder="Votre prénom">
+			<br>
+	
+		<input type="text" name="ContactMail" placeholder="Votre Email">
+			<br>
+	
+		<input type="text" name="ContactNuméro" placeholder="Votre Numéro">
+			<br>
+			<br>
+	<input type="submit" name="BtnValider" 
+	style="
+	background-color: orange;
+	font-size: 20px;
+	border-color: orange;
+	background-color: orange;
+	font-family: Didot, serif;
+	border-radius: 5px;
+	width: 150px
+	"> 
+
+</form>
+</section>
+
+<br>
+
+<!--  FOOTER -->
+
+<footer>
+	
+	<div>
+		<h2 id="TexteFooter">Travel Agency<strong id="Point">.</strong></h2>
+
+		<p id="CopyrightFooter"> Réaliser par 
+			<a href="https://portfolio-alkarshi-abdullrahman.alwaysdata.net/"
+				style="text-decoration: none;
+				color: orange;" > 
+				 Al karshi Abdullrahman </a>
+		Copyright 	&#169; 2023. Tout droits réservés. </p>
+	</div>
+
+
+
+	
+</footer>
+</body>
+</html>
